9693cb91cd6d6cc479341117e5347e624303a863
VishalKumarThakur7 <vishalchintujha@gmail.com>
2025-10-17 20:13:15 +0530

    Time: 2 ms (74.36%) | Memory: 42.4 MB (89.74%) - LeetSync

--- 3233-maximize-the-number-of-partitions-after-operations/maximize-the-number-of-partitions-after-operations.java
+++ 3233-maximize-the-number-of-partitions-after-operations/maximize-the-number-of-partitions-after-operations.java
@@ -1,72 +1,40 @@
 class Solution {
-    
-
-    private Map<List<Integer>, Integer> memo = new HashMap<>();
-    private String inputString;
-    private int maxDistinctChars;
-
-    public int maxPartitionsAfterOperations(String s, int k) {
-        this.inputString = s;
-        this.maxDistinctChars = k;
-        // Start DFS from position 0, empty bitmask, with 1 change allowed
-        return dfs(0, 0, 1);
-    }
-
-    /**
-     * Dynamic programming with memoization to find maximum partitions
-     * @param index - current position in string
-     * @param currentMask - bitmask representing distinct characters in current partition
-     * @param canChange - flag indicating if we can still change a character (1 = yes, 0 = no)
-     * @return maximum number of partitions from current state
-     */
-    private int dfs(int index, int currentMask, int canChange) {
-        // Base case: reached end of string
-        if (index >= inputString.length()) {
-            return 1;
+    public int maxPartitionsAfterOperations(String S, int k) {
+        if(k == 26) return 1;
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int[] left1 = new int[n], left2 = new int[n];
+        int[] partitions = new int[n];
+        int mask1 = 0, mask2 = 0, count = 1;
+        for(int i = 0; i < n; i++) {
+            int filter = 1 << (s[i] - 'a');
+            mask2 |= mask1 & filter;
+            mask1 |= filter;
+            if(Integer.bitCount(mask1) > k) {
+                mask1 = filter;
+                mask2 = 0;
+                count++;
+            }
+            left1[i] = mask1;
+            left2[i] = mask2;
+            partitions[i] = count;
         }
-      
-        // Check memoization
-        var memoKey = List.of(index, currentMask, canChange);
-        if (memo.containsKey(memoKey)) {
-            return memo.get(memoKey);
-        }
-      
-        // Get bitmask for current character
-        int charBit = 1 << (inputString.charAt(index) - 'a');
-      
-        // Try not changing current character
-        int newMask = currentMask | charBit;
-        int maxPartitions;
-      
-        if (Integer.bitCount(newMask) > maxDistinctChars) {
-            // Adding current char exceeds k distinct chars, start new partition
-            maxPartitions = dfs(index + 1, charBit, canChange) + 1;
-        } else {
-            // Continue with current partition
-            maxPartitions = dfs(index + 1, newMask, canChange);
-        }
-      
-        // Try changing current character (if we haven't used our change yet)
-        if (canChange > 0) {
-            // Try all 26 possible characters
-            for (int charIndex = 0; charIndex < 26; ++charIndex) {
-                newMask = currentMask | (1 << charIndex);
-              
-                if (Integer.bitCount(newMask) > maxDistinctChars) {
-                    // Changing to this char would exceed k distinct chars, start new partition
-                    maxPartitions = Math.max(maxPartitions, 
-                        dfs(index + 1, 1 << charIndex, 0) + 1);
-                } else {
-                    // Continue with current partition after change
-                    maxPartitions = Math.max(maxPartitions, 
-                        dfs(index + 1, newMask, 0));
-                }
+        int ans = count;
+        mask1 = mask2 = count = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            int filter = 1 << (s[i] - 'a');
+            mask2 |= mask1 & filter;
+            mask1 |= filter;
+            if(Integer.bitCount(mask1) > k) {
+                mask1 = filter;
+                mask2 = 0;
+                count++;
+            }
+            if(Integer.bitCount(mask1) == k) {
+                if((filter & mask2) != 0 && (filter & left2[i]) != 0 && Integer.bitCount(left1[i]) == k && (left1[i] | mask1) != (1 << 26) - 1) ans = Math.max(ans, count + partitions[i] + 2);
+                else if(mask2 != 0) ans = Math.max(ans, count + partitions[i] + 1);
             }
         }
-      
-        // Store result in memoization map
-        memo.put(memoKey, maxPartitions);
-        return maxPartitions;
+        return ans;
     }
 }
-
