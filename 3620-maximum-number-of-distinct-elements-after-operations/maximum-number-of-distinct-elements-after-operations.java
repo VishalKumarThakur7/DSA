df5e7f378bb7bbaa564d90c2c5e7e58840487981
VishalKumarThakur7 <vishalchintujha@gmail.com>
2025-10-18 21:12:29 +0530

    Time: 10 ms (97.24%) | Memory: 58.2 MB (53.79%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
@@ -1,34 +1,16 @@
-
-
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
-        int n = nums.length;
-        int[][] intervals = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            intervals[i][0] = nums[i] - k;  // start
-            intervals[i][1] = nums[i] + k;  // end
-        }
-        
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        long nextAvailable = Long.MIN_VALUE;
+        if (nums.length <= (k << 1) + 1) return nums.length;
+        Arrays.sort(nums);
         int distinct = 0;
-        
-        for (int[] interval : intervals) {
-            long start = interval[0];
-            long end = interval[1];
-            
-            if (nextAvailable < start) {
-                nextAvailable = start;
-            }
-            
-            if (nextAvailable <= end) {
+        int l = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int m = Math.max(l+1, nums[i]-k);
+            if (m <= nums[i]+k) {
                 distinct++;
-                nextAvailable++;
+                l = m;
             }
         }
-        
         return distinct;
     }
 }
