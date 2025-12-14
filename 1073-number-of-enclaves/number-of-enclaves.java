af9c340eab08c3e2131f2851754aa33bbb5bded2
VishalKumarThakur7 <vishalchintujha@gmail.com>
2025-12-14 16:24:11 +0530

    Time: 5 ms (99.78%) | Memory: 60.2 MB (46.58%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int numEnclaves(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
         for (int i = 0; i < n; i++) {
-            if (mat[i][0] == 1) 
-                dfs(mat, i, 0);
-            if (mat[i][m - 1] == 1)
-                dfs(mat, i, m - 1);
+            if (grid[i][0] == 1) 
+                dfs(grid, i, 0);
+            if (grid[i][m - 1] == 1)
+                dfs(grid, i, m - 1);
         }
 
         for (int i = 0; i < m; i++) {
-            if (mat[0][i] == 1) 
-                dfs(mat, 0, i);
-            if (mat[n - 1][i] == 1)
-                dfs(mat, n - 1, i);
+            if (grid[0][i] == 1) 
+                dfs(grid, 0, i);
+            if (grid[n - 1][i] == 1)
+                dfs(grid, n - 1, i);
         }
 
         int res = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                res += mat[i][j];
+                res += grid[i][j];
             }
         }
 
@@ -28,13 +28,13 @@ class Solution {
         
     }
 
-    static void dfs(int[][] mat, int r, int c) {
-        if (r < mat.length && r > -1 && c < mat[0].length && c > -1 && mat[r][c] == 1) {
-            mat[r][c] = 0;
-            dfs(mat, r + 1, c);
-            dfs(mat, r - 1, c);
-            dfs(mat, r, c + 1);
-            dfs(mat, r, c - 1);
+    static void dfs(int[][] grid, int r, int c) {
+        if (r < grid.length && r > -1 && c < grid[0].length && c > -1 && grid[r][c] == 1) {
+            grid[r][c] = 0;
+            dfs(grid, r + 1, c);
+            dfs(grid, r - 1, c);
+            dfs(grid, r, c + 1);
+            dfs(grid, r, c - 1);
         }
         
     }
